be9c53678d9a31270a8b78c5e53298abb8ef3711
Nate Parrott <nateparro2t@gmail.com>
2018-03-21 22:59:47 -0400

    bad work

--- A1/A1/ArticleCell.swift
+++ A1/A1/ArticleCell.swift
@@ -13,17 +13,27 @@ class ArticleCell: UICollectionViewCell {
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        layer.cornerRadius = 16
         layer.shadowColor = UIColor(white: 0.1, alpha: 1).cgColor
         layer.shadowOpacity = 0.1
         layer.shadowRadius = 20
         
-        webView.layer.cornerRadius = layer.cornerRadius
+        addSubview(clippedContainer)
+        clippedContainer.layer.cornerRadius = 16
+        clippedContainer.clipsToBounds = true
+        
         webView.isUserInteractionEnabled = false
-        contentView.addSubview(webView)
+        clippedContainer.addSubview(webView)
+        
+        title.backgroundColor = UIColor(white: 1, alpha: 0.9)
+        title.font = UIFont.boldSystemFont(ofSize: 14)
+        title.textAlignment = .center
+        title.textColor = UIColor(white: 0.1, alpha: 1)
+        clippedContainer.addSubview(title)
     }
     
+    let clippedContainer = UIView()
     let webView = WKWebView(frame: .zero)
+    let title = UILabel()
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
@@ -36,12 +46,15 @@ class ArticleCell: UICollectionViewCell {
             webView.loadHTMLString("", baseURL: nil)
             guard let urlString = a.amp_url, let url = URL(string: urlString) else { return }
             webView.load(URLRequest(url: url))
+            title.text = a.title
         }
     }
     
     override func layoutSubviews() {
         super.layoutSubviews()
-        layer.shadowPath = UIBezierPath(roundedRect: bounds, cornerRadius: layer.cornerRadius).cgPath
+        clippedContainer.frame = bounds
+        layer.shadowPath = UIBezierPath(roundedRect: bounds, cornerRadius: clippedContainer.layer.cornerRadius).cgPath
+        title.frame = CGRect(x: 0, y: 0, width: bounds.width, height: 24)
         webView.frame = bounds
     }
 }
